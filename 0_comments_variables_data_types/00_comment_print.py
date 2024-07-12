494e0b6d3755b06b8b26cb990f16f72d678d69ba
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-12 23:32:15 +0300

    Lesson 0 Comments print

--- 0_comments_variables_data_types/00_comment_print.py
+++ 0_comments_variables_data_types/00_comment_print.py
@@ -15,6 +15,11 @@ it doesn't matter,
 it's equivalent in Python
 '''
 
-
-
-
+# You can print most of data types
+print('Basic text')
+print(123)
+print(1.23)
+print(True)
+print(False)
+print('\t')
+print(None)
