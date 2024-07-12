54ead10d9dcf30cc25dade575b0901ba59e2f2f9
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-13 00:30:49 +0300

    Lesson 0 function numbers

---
+++ 0_comments_variables_data_types/03_number_functions.py
@@ -0,0 +1,17 @@
+# Absolute value
+print(abs(-1.6))
+
+# Convert int to float and vice versa
+print(int(56.78899))
+print(float(78))
+
+# If string is number, it may be converted to number
+print(int('6'))
+print(float('-7.89'))
+
+# Round with needed precision by maths rules
+print(round(1.44550, 3))
+
+# Guess the order of arithmetic (brackets -> ** -> *, /, % -> +, -)
+print((4 - 9) * 5 ** 2 + 7)
+print(9 / (2 + 4 * 3))
