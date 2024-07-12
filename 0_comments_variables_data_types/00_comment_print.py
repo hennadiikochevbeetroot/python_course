3f37a3a9a5723f1a00a8bea188b09504e2e05069
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-12 23:47:54 +0300

    Lesson 0 Comments print 1

--- 0_comments_variables_data_types/00_comment_print.py
+++ 0_comments_variables_data_types/00_comment_print.py
@@ -16,6 +16,9 @@ it's equivalent in Python
 '''
 
 # You can print most of data types
+# Ctrl + Space for autocompletion trigger
+# Ctrl + P for showing parameters list
+# Ctrl + Alt + L to trigger autoformatting
 print('Basic text')
 print(123)
 print(1.23)
---
+++ 0_comments_variables_data_types/01_print_options.py
@@ -0,0 +1,17 @@
+# You can print several things - (*values) param
+print(1, 2, 3, 4, 5)
+print(True, False)
+print('text1', 'text2', 'text3')
+
+# Print with separator
+print(9, 8, 7, 6, 5, sep=':::')
+
+# Print with custom ending (by default it's new line)
+print('Line 1 without break', end='.')
+print('Line 2 on first line')
+
+# We can print nothing, then we will just have new line
+print()
+
+# Getting help even here - equivalent to docs in PyCharm
+help(print)
