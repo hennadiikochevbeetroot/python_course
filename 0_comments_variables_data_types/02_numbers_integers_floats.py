e7068d205a9425c79fee22f8d1aed8f46a7f802b
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-13 00:15:52 +0300

    Lesson 0 numbers

---
+++ 0_comments_variables_data_types/02_numbers_integers_floats.py
@@ -0,0 +1,32 @@
+# We have int and floats, + and -
+print(1)
+print(-2)
+print(0)
+print(3.14)
+print(-56.78)
+
+# We can get types
+print(type(1))
+print(type(0.0))
+print(type(4.56))
+
+# Basic maths
+print(2 + 1)
+print(5 - 2)
+print(9 / 3)
+print(13 // 2)
+
+# / float division, // rounding down int division
+print(9 / 4, type(9 / 4))
+print(9 // 4, type(9 // 4))
+
+print(8 % 5)    # get remainder
+print(3 ** 2)   # raise to power
+
+
+# Negative and positive changing possibility
+print(--1)
+print(+-13)
+
+
+
