f4732cd22d707b6dc5b62432e31e681e98710ed0
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-18 22:14:11 +0300

    Lesson 1 couple livecoding tasks

--- 1_bool_conditions_while/08_livecoding_task1.py
+++ 1_bool_conditions_while/08_livecoding_task1.py
@@ -2,3 +2,8 @@
 # Write a Python program to convert temperatures
 # to and from Celsius, Fahrenheit.
 # Formula : °F = (°C × 9/5) + 32
+
+celsius_temperature = 25
+fahrenheit_temperature = celsius_temperature * 9 / 5 + 32
+
+print(fahrenheit_temperature)
--- 1_bool_conditions_while/09_livecoding_task2.py
+++ 1_bool_conditions_while/09_livecoding_task2.py
@@ -10,3 +10,25 @@
 # * * *
 # * *
 # *
+
+star_counter = 1
+tree_size = 20
+star_symbol = '*'
+while star_counter < tree_size:
+    inner_counter = 0
+    while inner_counter < star_counter:
+        print(star_symbol, end=' ')
+        inner_counter += 1
+
+    star_counter += 1
+    print()
+
+while star_counter >= 1:
+    inner_counter = 0
+    while inner_counter < star_counter:
+        print(star_symbol, end=' ')
+        inner_counter += 1
+
+    star_counter -= 1
+    print()
+
