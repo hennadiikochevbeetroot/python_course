bedb25fa3578ea38fdf17832e2ed1c5b91531a76
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-07-30 23:08:41 +0300

    Lesson Exceptions live changes

--- 5_exceptions_modules_stdlib/6_hashlib_module.py
+++ 5_exceptions_modules_stdlib/6_hashlib_module.py
@@ -2,6 +2,13 @@ import hashlib
 
 some_string = 'data to encrypt'
 
+email1 = 'hennadii@example.com'
+email2 = 'hennadii@example.com'
+
+# Equal hash results
+print(hashlib.md5(email1.encode()).hexdigest())
+print(hashlib.md5(email2.encode()).hexdigest())
+
 md5_string = hashlib.md5(some_string.encode())
 print('MD5 hash: ', md5_string.hexdigest())
 
--- 5_exceptions_modules_stdlib/7_itertools_module.py
+++ 5_exceptions_modules_stdlib/7_itertools_module.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 
 # INFINITE GENERATORS
 
@@ -10,10 +11,25 @@ print(next(c))  # Output:1
 print(next(c))  # Output:2
 print(next(c))  # Output:3
 
+# counter = 0
+# while True:
+#     counter += 1
+
+
+for counter in itertools.count():
+    print(counter)
+    if counter == 100:
+        break
+
 l1 = [1, 2, 3]
 # using list iterable as an argument in itertools.cycle()
 l2 = itertools.cycle(l1)
 print(l2)  # Output:<itertools.cycle object at 0x02F794E8>
+
+for element in itertools.cycle(l1):
+    if random.random() > 0.9:
+        print('Winner ', element)
+        break
 
 count = 0
 for i in l2:
@@ -29,11 +45,16 @@ l1 = itertools.repeat(2)
 print(next(l1))  # Output:2
 print(next(l1))  # Output:2
 
+# for value in itertools.repeat('some string'):
+#     print(value)
+#     if
+
+
 l1 = itertools.chain(["red", "blue"], [1, 2, 3], "hello")
 # Returns an iterator object
 print(l1)  # Output:<itertools.chain object at 0x029FE4D8>
 # converting iterator object to list object
 print(list(l1))  # Output:['red', 'blue', 1, 2, 3, 'h', 'e', 'l', 'l', 'o']
 
-l2 = itertools.permutations([3, 2, 1])
+l2 = itertools.permutations([3, 2, 1])  # 3! = 1 * 2 * 3 = 6
 print(list(l2))  # Output:[(3, 2, 1), (3, 1, 2), (2, 3, 1), (2, 1, 3), (1, 3, 2), (1, 2, 3)]
