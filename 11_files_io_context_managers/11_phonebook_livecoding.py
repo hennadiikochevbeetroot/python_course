be5aaef924375b94544643fbfdce3ed05320a6b3
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-01 00:55:36 +0300

    11 - livecoding

---
+++ 11_files_io_context_managers/11_phonebook_livecoding.py
@@ -0,0 +1,23 @@
+# Functionality of Phonebook application:
+#
+# An option to exit the program
+# Add new entries
+# Search by first name
+# Search by last name
+# Search by last name
+# Search by telephone number
+# Search by city or state
+# Delete a record for a given telephone number
+# Update a record for a given telephone number
+
+# This program should operate on the console.
+# It should display all the choices of available
+# commands when the program loads.
+
+
+# Key points:
+# 1. all program logic should be split to different functions;
+#
+# 2. store all entries in the dict, where
+# key is the full name (tuple with length 2 - first name, last name)
+# and value another tuple with (all other data);
