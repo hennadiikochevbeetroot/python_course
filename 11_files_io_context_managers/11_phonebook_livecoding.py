b1cbbbc655bcaa5112fe839465914703d26f628e
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-02 00:41:14 +0300

    Lesson 11 - phonebook app homework

--- 11_files_io_context_managers/11_phonebook_livecoding.py
+++ 11_files_io_context_managers/11_phonebook_livecoding.py
@@ -1,30 +1,34 @@
-# Functionality of Phonebook application:
+# Functionality of Phonebook CLI application:
 #
 # 0. An option to exit the program
 # 1. Add new entries
 # 9. Print contents of dictionary
+#
+# BELOW IS YOUR HOMEWORK - GIVE IT A TRY
+# Bonus: try to split this program into separate files and modules
+# separate files for constants, business logic, etc
+#
 # Search by first name
 # Search by last name
-# Search by last name
 # Search by telephone number
-# Search by city or state
+# Search by city or country
 # Delete a record for a given telephone number
 # Update a record for a given telephone number
-
+#
 # This program should operate on the console.
 # It should display all the choices of available
 # commands when the program loads.
-
-
+#
 # Key points:
 # 1. all program logic should be split to different functions;
-#
 # 2. store all entries in the dict, where
 # key is the full name (tuple with length 2 - first name, last name)
-# and value another tuple with (all other data);
+# and value another tuple with (all other data):
 # {
 #     ('John', 'Smith'): ('+12423535', 'Minnesota', 'US')
 # }
+
+from typing import Callable
 
 HELP_MESSAGE = """
 Welcome to PhoneBook.
@@ -35,6 +39,35 @@ Options:
 """
 
 POSSIBLE_OPTIONS = (0, 1, 9)
+
+Phonebook = dict[tuple[str, str], tuple[str, str, str]]
+
+
+def exit_program() -> None:
+    quit(0)
+
+
+def add_entry(phonebook: Phonebook) -> None:
+    first_name = input('Enter first name: ')
+    last_name = input('Enter last name: ')
+    phone = input('Enter phone: ')
+    city = input('Enter city: ')
+    country = input('Enter country: ')
+
+    key = (first_name, last_name)
+    value = (phone, city, country)
+    phonebook[key] = value
+
+
+def print_phonebook(phonebook: Phonebook) -> None:
+    for (first_name, last_name), (phone, city, country) in phonebook.items():
+        print(f'Fullname: {first_name} {last_name}; Place: {city} {country}; Phone {phone}')
+
+
+OPTION_TO_FUNCTION: dict[int, Callable] = {
+    1: add_entry,
+    9: print_phonebook,
+}
 
 
 def validate_option(user_input: str) -> tuple[bool, int | None]:
@@ -47,6 +80,7 @@ def validate_option(user_input: str) -> tuple[bool, int | None]:
 
 def phonebook_app() -> None:
     print(HELP_MESSAGE)
+    phonebook: Phonebook = {}
     while True:
         user_input = input('Choose an option: ')
         is_valid, option = validate_option(user_input)
@@ -55,7 +89,10 @@ def phonebook_app() -> None:
             print(f'Incorrect option, possible are: {possible_options}')
             continue
 
-        print('Correct option.')
+        if option == 0:
+            quit(option)
+
+        OPTION_TO_FUNCTION[option](phonebook)
 
 
 if __name__ == '__main__':
