4d8db4afd3c5fc74fc55a48c8454f04596273ab8
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-01 21:57:08 +0300

    11 - phonebook step 1

--- 11_files_io_context_managers/11_phonebook_livecoding.py
+++ 11_files_io_context_managers/11_phonebook_livecoding.py
@@ -1,7 +1,8 @@
 # Functionality of Phonebook application:
 #
-# An option to exit the program
-# Add new entries
+# 0. An option to exit the program
+# 1. Add new entries
+# 9. Print contents of dictionary
 # Search by first name
 # Search by last name
 # Search by last name
@@ -21,3 +22,37 @@
 # 2. store all entries in the dict, where
 # key is the full name (tuple with length 2 - first name, last name)
 # and value another tuple with (all other data);
+# {
+#     ('John', 'Smith'): ('+12423535', 'Minnesota', 'US')
+# }
+
+HELP_MESSAGE = """
+Welcome to PhoneBook.
+Options:
+0. An option to exit the program
+1. Add new entries
+9. Print contents of dictionary
+"""
+
+POSSIBLE_OPTIONS = (0, 1, 9)
+
+
+def validate_option(user_input: str) -> bool:
+    user_input = user_input.strip()
+    return user_input.isdigit() and int(user_input) in POSSIBLE_OPTIONS
+
+
+def phonebook_app() -> None:
+    print(HELP_MESSAGE)
+    while True:
+        user_input = input('Choose an option: ')
+        if not validate_option(user_input):
+            possible_options = ', '.join([str(option) for option in POSSIBLE_OPTIONS])
+            print(f'Incorrect option, possible are: {possible_options}')
+            continue
+
+        print('Correct option.')
+
+
+if __name__ == '__main__':
+    phonebook_app()
