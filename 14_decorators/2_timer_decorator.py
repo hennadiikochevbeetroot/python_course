df1d91ff33efaf13f85fb2cc50ae7f222e595f68
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-08 00:39:46 +0300

    Lesson 14 decorators

---
+++ 14_decorators/2_timer_decorator.py
@@ -0,0 +1,30 @@
+import datetime
+import random
+import time
+from functools import wraps
+from typing import Callable
+
+
+def timer(func: Callable) -> Callable:
+    @wraps(func)
+    def inner(*args, **kwargs):
+        start_time = datetime.datetime.now()
+        func(*args, **kwargs)
+        end_time = datetime.datetime.now()
+        print('Diff time: ', end_time - start_time)
+
+    return inner
+
+
+@timer
+def sleep_random():
+    """Sleeps some time"""
+    print('Sleeping')
+    time.sleep(random.randint(1, 5))
+    print('End sleeping')
+
+
+print(sleep_random.__name__)
+print(sleep_random.__doc__)
+
+sleep_random()
---
+++ 14_decorators/3_timer_parameter_decorator.py
@@ -0,0 +1,39 @@
+import datetime
+import random
+import time
+from functools import wraps
+from typing import Callable
+
+
+def timer(surround: bool = True):
+    def timer_deco(func: Callable):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            start_time = datetime.datetime.now()
+            func(*args, **kwargs)
+            end_time = datetime.datetime.now()
+            diff_time = end_time - start_time
+
+            message = f'Diff time is: {diff_time}'
+            if surround:
+                message = f'**********\n{message}\n**********\n'
+
+            print(message)
+
+        return inner
+
+    return timer_deco
+
+
+@timer(surround=True)
+def sleep_random():
+    """Sleeps some time"""
+    print('Sleeping')
+    time.sleep(random.randint(1, 5))
+    print('End sleeping')
+
+
+print(sleep_random.__name__)
+print(sleep_random.__doc__)
+
+sleep_random()
