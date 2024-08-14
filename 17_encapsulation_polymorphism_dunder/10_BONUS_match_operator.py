50f7b0b3034cd643a77bb5e5872523ed8020e199
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-15 01:02:34 +0300

    Lesson 17 match bonus

---
+++ 17_encapsulation_polymorphism_dunder/10_BONUS_match_operator.py
@@ -0,0 +1,32 @@
+def get_number_explanation_conditions(number: int) -> str:
+    description = None
+    if number == 666:
+        description = 'Devil number'
+    elif number == 42:
+        description = 'Answer for everything'
+    elif number == 7:
+        description = 'Prime number'
+    else:
+        description = 'Just a number'
+
+    return f'Number {number} is {description}'
+
+
+def get_number_explanation_match(number: int) -> str:
+    description = None
+    match number:
+        case 666:
+            description = 'Devil number'
+        case 42:
+            description = 'Answer for everything'
+        case 7:
+            description = 'Prime number'
+        case _:
+            description = 'Just a number'
+
+    return f'Number {number} is {description}'
+
+
+if __name__ == '__main__':
+    number = int(input('Please enter a number: '))
+    print(get_number_explanation_match(number))
