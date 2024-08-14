0ec534ab4b08c39fb92cce18ce73fdf0e0b3dd51
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-15 00:09:25 +0300

    Lesson 17 private

---
+++ 17_encapsulation_polymorphism_dunder/04_private_fields_methods.py
@@ -0,0 +1,26 @@
+import math
+
+
+class Cylinder:
+    def __init__(self, height: int | float, radius: float):
+        self.__height = height  # Making field private - non-accessible by default
+        self.__radius = radius
+
+    def __rounded_pi(self):
+        return round(math.pi, 2)
+
+    @property
+    def volume(self):
+        return self.__rounded_pi() * self.__radius ** 2 * self.__height
+
+
+cylinder = Cylinder(4, 5)
+print(cylinder.volume)
+# Below fields and method are not intended to be used, so they are not accessible
+# print(cylinder.__height) - wouldn't work
+# print(cylinder.__radius) - wouldn't work
+# print(cylinder.__rounded_pi()) - wouldn't work
+
+print('Hack which allows to get private properties')
+print(cylinder._Cylinder__height)
+print(cylinder._Cylinder__radius)
