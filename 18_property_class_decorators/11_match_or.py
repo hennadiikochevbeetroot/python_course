1128883cdf7374156fbf8c3edd2e8e2d0237274c
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-17 02:04:23 +0300

    Lesson 18 match or

---
+++ 18_property_class_decorators/11_match_or.py
@@ -0,0 +1,18 @@
+
+
+def get_number_type(natural_less_than_10: int) -> str:
+    desc = None
+    match natural_less_than_10:
+        case 3 | 5 | 7:
+            desc = 'Prime number'
+        case 2 | 4 | 6 | 8:
+            desc = 'Even number'
+        case 1:
+            desc = 'Exactly one'
+        case _:
+            desc = 'Other number'
+
+    return desc
+
+
+print(get_number_type(4))
