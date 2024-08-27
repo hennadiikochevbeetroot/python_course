c6753e476f0d842559295f41022a26104d9c4059
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-08-27 11:46:14 +0300

    Lesson 21 env example

---
+++ 21_context_managers_extended/05_environment_cm.py
@@ -0,0 +1,30 @@
+import os
+from contextlib import contextmanager
+
+
+@contextmanager
+def temp_environment(**envs):
+    """Temporarily set environment variables inside the context … """
+
+    # Save old vars and replace them with new ones
+    original_env = {k: os.environ.get(k) for k in envs}
+    os.environ.update(envs)
+
+    try:
+        yield  # Simply start what's inside of CM block
+    finally:
+        # Return old env vars back
+        for k, v in original_env.items():
+            if v is None:
+                del os.environ[k]
+            else:
+                os.environ[k] = v
+
+
+if __name__ == '__main__':
+    print('Current domain is: ', os.environ.get('USERDOMAIN'))
+
+    with temp_environment(USERDOMAIN='NEWDOMAINTEMPORARY'):
+        print('Inside temporary: ', os.environ.get('USERDOMAIN'))
+
+    print('Back outside: ', os.environ.get('USERDOMAIN'))
