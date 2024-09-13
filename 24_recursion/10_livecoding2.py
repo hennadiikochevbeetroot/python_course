eb58dccac03454bbd7e09b4ff2f1f6a81e3887e4
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-09-14 00:53:30 +0300

    Lesson 24 livecoding

---
+++ 24_recursion/10_livecoding2.py
@@ -0,0 +1,11 @@
+# In the sys module there is a function sys.setrecursionlimit and sys.getrecursionlimit.
+# These two functions set and get the recursion limit.
+# import sys
+# sys.setrecursionlimit(905)
+# sys.getrecursionlimit() # returns 905
+# sys.setrecursionlimit(89)
+# sys.getrecursionlimit() # returns 89
+#
+# Task:
+# Write a function ```fetch_recursion_limit```
+# that acts the same as ```sys.getrecursionlimit``` without importing any libraries.
---
+++ 24_recursion/10_livecoding3.py
@@ -0,0 +1,3 @@
+# Given a list of unique integers, write a Python function
+# to return all possible subsets (the powerset) of the list.
+# The solution should be generated recursively.
