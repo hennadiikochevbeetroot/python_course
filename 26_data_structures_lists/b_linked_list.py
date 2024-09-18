c59deae724657cee12b45f3dd6755b3f99f5a615
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-09-18 23:55:48 +0300

    Lesson 26 add right

--- 26_data_structures_lists/b_linked_list.py
+++ 26_data_structures_lists/b_linked_list.py
@@ -13,6 +13,15 @@ class LinkedList:
     def add_left(self, value: Any) -> None:
         self.root = Node(value, self.root)
 
+    def add_right(self, value: Any) -> None:
+        if self.root is None:
+            self.root = Node(value, None)
+        else:
+            current = self.root
+            while current.next is not None:
+                current = current.next
+            current.next = Node(value, None)
+
     @property
     def size(self) -> int:
         count = 0
@@ -31,6 +40,19 @@ class LinkedList:
             current = current.next
 
         return False
+
+    def index(self, value: Any) -> int:
+        idx = 0
+        current = self.root
+        while current is not None:
+            if current.value == value:
+                return idx
+
+            current = current.next
+            idx += 1
+
+        # Not found
+        return -1
 
     def remove(self, value: Any) -> None:
         previous, current = None, self.root
@@ -56,7 +78,6 @@ class LinkedList:
         return result + 'NULL(END)'
 
 
-# TODO: add_right, search_position methods
 # TODO: passing list to constructor
 
 if __name__ == "__main__":
@@ -66,6 +87,7 @@ if __name__ == "__main__":
     linked_list.add_left(234)
     linked_list.add_left(True)
     linked_list.add_left(45.6)
+    linked_list.add_right('rightest')
 
     print(linked_list)
     print(linked_list.size)
@@ -73,3 +95,5 @@ if __name__ == "__main__":
 
     linked_list.remove(234)
     print(linked_list)
+    print(linked_list.index('dog'))
+    print(linked_list.index(234))
