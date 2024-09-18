033289272a636e7104c4d8d6fd2d79fad8ee3d48
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-09-19 00:55:11 +0300

    Lesson 26 dll

---
+++ 26_data_structures_lists/d_doubly_linked_list.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import Any
+
+
+class Node:
+    def __init__(self, value: Any, prev: Node | None = None, next: Node | None = None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.root: Node | None = None
+
+    # add_right
+    def append(self, value: Any) -> None:
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            current = self.root
+            while current.next is not None:
+                current = current.next
+            current.next = Node(value, prev=current)
+
+    # add_left
+    def prepend(self, value: Any) -> None:
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self.root.prev = Node(value, next=self.root)
+            self.root = self.root.prev
+
+    def remove(self, value: Any) -> None:
+        if self.root is None:
+            return
+
+        current = self.root
+        while current is not None:
+            if current.value == value:
+                if current.prev is not None:
+                    current.prev.next = current.next
+                if current.next is not None:
+                    current.next.prev = current.prev
+                if current == self.root:
+                    self.root = current.next
+                return
+
+            current = current.next
+
+    def print_forward(self):
+        print('Doubly Linked List (forward):')
+        current = self.root
+        while current is not None:
+            print(current.value, end=' <-> ' if current.next is not None else '\n')
+            current = current.next
+
+    def print_backward(self):
+        print('Doubly Linked List (backward):')
+        current = self.root
+        while current.next is not None:
+            current = current.next
+
+        while current is not None:
+            print(current.value, end=' <-> ' if current.prev else '\n')
+            current = current.prev
+
+
+if __name__ == '__main__':
+    doubly_linked_list = DoublyLinkedList()
+    doubly_linked_list.append(10)
+    doubly_linked_list.prepend(5)
+    doubly_linked_list.append(20)
+    doubly_linked_list.append(40)
+    doubly_linked_list.prepend(1)
+
+    doubly_linked_list.print_forward()
+    doubly_linked_list.remove(10)
+
+    doubly_linked_list.print_forward()
+    doubly_linked_list.print_backward()
