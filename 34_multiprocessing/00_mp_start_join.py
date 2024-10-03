355c39c603b60a082ec0a9520b312cc24a8fc2f9
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-03 16:41:23 +0300

    Lesson 34 mp start join

---
+++ 34_multiprocessing/00_mp_start_join.py
@@ -0,0 +1,27 @@
+import multiprocessing
+import os
+import random
+import string
+
+
+def random_string(length=5):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+def business_logic(param1: str, param2: str):
+    print(f'Parent: {os.getppid()}, process: {os.getpid()} - Business logic with param1: {param1}, param2: {param2}')
+
+
+if __name__ == '__main__':
+    processes = []
+    for count in range(5):
+        param1, param2 = random_string(), random_string()
+        process = multiprocessing.Process(target=business_logic, args=(param1, param2))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print('All processes finished')
