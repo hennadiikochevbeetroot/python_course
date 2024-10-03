da003a1a204937748a2bf2fb15f65049c6d11771
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-03 15:42:41 +0300

    Lesson 33 thread event

---
+++ 33_threading/07_threads_events.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+event = threading.Event()
+
+
+def worker():
+    print("[Worker] Waiting for the setup to complete...")
+    event.wait()
+    print("[Worker] Setup complete, starting processing...")
+    time.sleep(2)
+    print("[Worker] Processing done.")
+
+
+def setup():
+    print("[Main] Performing setup...")
+    time.sleep(3)
+    print("[Main] Setup is complete. Notifying worker...")
+    event.set()
+
+
+setup_thread = threading.Thread(target=setup)
+setup_thread.start()
+
+worker_thread = threading.Thread(target=worker)
+worker_thread.start()
+
+for thread in [setup_thread, worker_thread]:
+    thread.join()
+
+print("Both threads have completed.")
