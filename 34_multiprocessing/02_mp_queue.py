58f082477be1444b82ee66f012c6ce58e552b8b4
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-03 17:06:20 +0300

    Lesson 34 mpqueue

---
+++ 34_multiprocessing/02_mp_queue.py
@@ -0,0 +1,38 @@
+import multiprocessing
+import random
+import time
+
+
+def square(numbers: list[int], result_queue: multiprocessing.Queue):
+    for number in numbers:
+        result = number ** 2
+        result_queue.put(result)
+    # Final value
+    result_queue.put(None)
+
+
+def read_squared(result_queue: multiprocessing.Queue):
+    while True:
+        result = result_queue.get()
+        if result is None:  # Check for the final value
+            break
+
+        print(result)
+
+
+def main():
+    numbers = list(range(10))
+    queue = multiprocessing.Queue()
+
+    square_process = multiprocessing.Process(target=square, args=(numbers, queue))
+    read_process = multiprocessing.Process(target=read_squared, args=(queue,))
+
+    square_process.start()
+    read_process.start()
+
+    square_process.join()
+    read_process.join()
+
+
+if __name__ == '__main__':
+    main()
