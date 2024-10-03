58b53cd766a56a5d055e42592440414e28c3db29
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-03 17:19:20 +0300

    Lesson 34 pool

---
+++ 34_multiprocessing/04_mp_pool.py
@@ -0,0 +1,28 @@
+import multiprocessing
+
+
+def square(number: int):
+    return number ** 2
+
+
+def main():
+    numbers = list(range(10))
+
+    # Create a Pool with a number of worker processes
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        single_result = pool.apply_async(square, (5,))
+        print('Single process result: ', single_result.get())
+
+        multiple_results = pool.map(square, numbers)
+        print("Original numbers:", numbers)
+        print('Multiple processes result:', multiple_results)
+
+        iterator_results = pool.imap(square, numbers)
+        print(next(iterator_results))
+        print(next(iterator_results))
+        print(next(iterator_results))
+        print(next(iterator_results))
+
+
+if __name__ == '__main__':
+    main()
